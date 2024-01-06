f02a57285c97d261570e11205b1e78aec1e4cba7
Vi13333 <151737696+Vi13333@users.noreply.github.com>
2024-01-06 23:19:24 +0200

    ✅finished

--- index.js
+++ index.js
@@ -2,11 +2,51 @@ function refreshWeather(response){
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
+
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
 
     cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
+
+    cityElement.classList.remove("hidden");
+    document.querySelector("#about").classList.remove("hidden");
+    document
+      .querySelector(".weather-app-temperature")
+      .classList.remove("hidden");
+
 }
 
+    function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
 
 function searchCity(city){
     let apiKey = "5a40eb0c0d6668b4da5oc31d5aa39t3f";
@@ -18,6 +58,11 @@ function searchCity(city){
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
+
+    document.querySelector("#city").classList.add("hidden");
+    document.querySelector("#about").classList.add("hidden");
+    document.querySelector(".weather-app-temperature").classList.add("hidden");
+
     
     searchCity(searchInput.value);
 }
