7b547ca3b0a45d653bfdb4b557794e5839ec07fe
Vi13333 <151737696+Vi13333@users.noreply.github.com>
2024-01-06 20:49:43 +0200

    added searching

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-function handleSearchSubmit(event){
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
+function refreshWeather(response){
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
 }
 
 
-let serchFormElement = document.querySelector("#search-form");
-serchFormElement.addEventListener("submit", handleSearchSubmit);
+function searchCity(city){
+    let apiKey = "5a40eb0c0d6668b4da5oc31d5aa39t3f";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
+
+function handleSearchSubmit(event){
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    
+    searchCity(searchInput.value);
+}
+
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
